e516144f478df89dc919f0dc78cf512aceceb4cd
Alan Andrade <alan.andradec@gmail.com>
2015-03-25 15:06:10 -0700

    Prototype with random selection

--- RestaurantPicker/RestaurantPicker/ViewController.swift
+++ RestaurantPicker/RestaurantPicker/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        result.hidden = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +20,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var pick1: UITextField!
+    @IBOutlet weak var pick2: UITextField!
+    @IBOutlet weak var pick3: UITextField!
 
+    @IBAction func pick(sender: UIButton) {
+        let array = [pick1, pick2, pick3]
+        let randomIndex = Int(arc4random_uniform(UInt32(array.count)))
+        result.text = array[randomIndex].text
+        result.hidden = false
+    }
+    
+    @IBOutlet weak var result: UILabel!
 }
 
